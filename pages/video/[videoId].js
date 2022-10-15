466a748cbbc6a58476dfef7f051f6462db9fdb86
Solomon S <schinoyerem007@gmail.com>
2022-10-15 22:07:50 +0100

    Feature: added modal using react-modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,12 +1,69 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+import clsx from "classnames";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
 
   const { videoId } = router.query;
 
-  return <div>Videos {videoId}</div>;
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1999-01-01",
+    description:
+      "A big red dog that is super cute, ca he get any bigger? A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger? A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?A big red dog that is super cute, ca he get any bigger?",
+    channelTitle: "Paramont Pictures",
+    viewCount: 1000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        onRequestClose={() => router.back()}
+        overlayClassName={styles.overlay}
+        className={styles.modal}
+        contentLabel="Watch the video"
+      >
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="100%"
+          className={styles.videoPlayer}
+          height="360"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com&controls=0&rel=0`}
+          frameBorder="0"
+        ></iframe>
+        {/* M7lc1UVf-VE */}
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Video;
